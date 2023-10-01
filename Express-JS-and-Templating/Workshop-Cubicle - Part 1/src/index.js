cfd24137eb60c0adfd222fa7ecad6e147d16f4be
Tanyo Ch <>
2023-10-01 08:35:20 +0300

    CUP-P1-Initialize handlebars, setup first default view

--- Express-JS-and-Templating/Workshop-Cubicle - Part 1/src/index.js
+++ Express-JS-and-Templating/Workshop-Cubicle - Part 1/src/index.js
@@ -1,9 +1,17 @@
-const express = require('express'); //регистрираме експрес-а енд пойнт
+const express = require('express'); //импортираме експрес-а енд пойнт
+const handlebars = require('express-handlebars'); //импортираме 
+
 const app = express(); //викаме експрес функцията 
+
+app.engine("hdb", handlebars.engine({extname: "hdb"})); //handlebars configuration
+app.set("view engine", "hdb"); //сетваме view engin-a hdb на application-a 
+app.set("views", "src/views"); // сетваме hdb към коя папка да бъде 
+
 const PORT = 8080; //константа с порта
 
-app.get('/', (req, res) =>{
-    res.send('Hello from express server');
+app.get("/", (req, res) =>{
+    //res.send('Hello from express server');
+    res.render("index");
 })
 
-app.listen(PORT, ()=> console.log('Servers is running on port ${PORT}...')); //логваме да виждаме кога работи сървъра
+app.listen(PORT, () => console.log(`Servers is running on port ${PORT}...`)); //логваме да виждаме кога работи сървъра
