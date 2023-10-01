f17018e5511ba92c7b41039756097f46eb54ffc8
Tanyo Ch <>
2023-10-01 21:34:02 +0300

    CUB-P1 hardcode - data

--- Express-JS-and-Templating/Workshop-Cubicle - Part 1/src/services/cubeService.js
+++ Express-JS-and-Templating/Workshop-Cubicle - Part 1/src/services/cubeService.js
@@ -1,7 +1,23 @@
  
  const uniqid = require("uniqid");
  
- const cubes = [];
+ const cubes = [
+    {
+        id: '62209f8ln7sc0rt',
+        name: 'Cube 1',
+        description: 'test',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMRS4ZW4gPHEn-zNHpIyfTRMs4qhXJGjoRR4gcsMR3Aw&s',
+        difficultyLevel: 1
+      },
+      {
+        id: '62209f8ln7sc0rt',
+        name: 'Cube 2',
+        description: 'test 2',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMRS4ZW4gPHEn-zNHpIyfTRMs4qhXJGjoRR4gcsMR3Aw&s',
+        difficultyLevel: 2
+      }
+
+ ];
 
 //exports.create = (name, description, imageUrl, difficultyLevel) => {
 
